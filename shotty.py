5532b1205aa4a0d46a601c15a57c99304d3d5770
Dhankhar <anmol.dhankhar@accenture.com>
2018-05-24 11:01:27 +0530

    updated scripts to list instances

--- shotty.py
+++ shotty.py
@@ -1,8 +1,17 @@
 import boto3
+import click
+
+session=boto3.Session(profile_name='shotty')
+ec2=session.resource('ec2')
+
+@click.command()
+def list_instances():
+   for i in ec2.instances.all():
+      print(','.join((
+	        i.instance_id,
+			i.placement['AvailabilityZone'],
+			i.instance_type,
+			i.state['Name'])))
 
 if __name__=='__main__':
-  session=boto3.Session(profile_name='shotty')
-  ec2=session.resource('ec2')
- 
-  for i in ec2.instances.all():
-      print(i)
+  list_instances()
